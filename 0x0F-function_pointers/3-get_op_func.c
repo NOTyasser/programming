7036096896de7e1b5021d061a37d40e5fc210345
Notyasser <showhad55@gmail.com>
2023-04-19 17:16:55 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,10 +1,9 @@
-#include "3-calc.h"
-
+#include"3-calc.h"
 /**
- * get_op_func - get the operator and redirected to the func.
- * @s: the operartor
+ * get_op_func - get the operator and redirect to the func.
+ * @s: the operator
 (* a blank line
-* Description: get the operator and redirected to the fun.)?
+* Description: get the operator and redirect to the func.)?
 (* section header: 3-calc.h)*
 * Return: return null or the function to do.
 */
@@ -12,23 +11,23 @@
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    i = 0;
-    while (i < 5)
-    {
-	    if (strcmp(s, ops[i].op) == 0)
-	    {
-		    return (*(ops[i]).f);
-	    }
-	    i++;
-    }
-    return (NULL);
+	i = 0;
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (*(ops[i]).f);
+		}
+		i++;
+	}
+	return (NULL);
 }
