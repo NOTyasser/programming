a300d239524b09078199cdf60749424113a5b2e3
Notyasser <showhad55@gmail.com>
2023-05-02 17:42:10 -0700

    deletes the head node of a listint_t linked list

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * pop_listint - function that deletes the head node of a listint_t linked list
+ * pop_listint - returns value of head node and removes it
  * @head: head of the list
  * Return:  the head node’s data (n)
  */
