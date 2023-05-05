e76b6f0854c45a2022381212f227555a7934a156
Notyasser <showhad55@gmail.com>
2023-05-04 17:03:59 -0700

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,7 +4,7 @@
 /**
  * binary_to_uint - function that converts a binary number to an unsigned int
  * 
- * @b: char string
+ * @b: char the string
  * 
  * Return: the converted number, or 0 if there is one or more chars in the string
  */
