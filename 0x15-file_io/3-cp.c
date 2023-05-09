3aa388bb525ad8aebf7876e0c332578fa8064387
Notyasser <showhad55@gmail.com>
2023-05-09 16:12:55 -0700

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,10 +3,9 @@
 
 /**
  * error_file - checks if files can be opened.
- * @file_from: file from.
- * @file_to: file to.
+ * @file_from: file_from.
+ * @file_to: file_to.
  * @argv: arguments vector.
- *
  * Return: no return.
  */
 void error_file(int file_from, int file_to, char *argv[])
